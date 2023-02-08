f3b2768883b0caf3d34c7762da6b7cb07fafedc8
afc163 <afc163@gmail.com>
2023-02-08 11:38:12 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ ZSH=$HOME/.oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 rm -rf dotfiles
-git clone git://github.com/afc163/dotfiles.git dotfiles
+git clone https://github.com/afc163/dotfiles.git --depth=1
 cd dotfiles
 
 cp -f gitconfig $HOME/.gitconfig
